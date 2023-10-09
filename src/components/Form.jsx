fbeb77846d5b4fa8c8db7dd52cb672684c566e3c
CarlosCrisonG <carloscrison@gmail.com>
2023-10-09 05:13:51 -0500

    Paciente component created

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,7 +4,7 @@ function Form() {
       <div className="md:w-1/2 lg:w-2/5">
         <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
 
-        <p className="text-lg mt-5 text-center mb-10">
+        <p className="text-xl mt-5 mb-10 text-center">
           Añade Pacientes y {""}
           <span className="text-indigo-600 font-bold">Adminístralos</span>
         </p>
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,26 @@
+//rfec
+function Paciente() {
+    return (
+        <>
+            <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+                <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+                    <span className="font-normal normal-case">Hook</span>
+                </p>
+                <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+                    <span className="font-normal normal-case">Juan</span>
+                </p>
+                <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+                    <span className="font-normal normal-case">user@email.com</span>
+                </p>
+                <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+                    <span className="font-normal normal-case">10 de Diciembre de 2022</span>
+                </p>
+                <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
+                    <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore minima commodi, recusandae vitae delectus omnis voluptas fugiat aliquam nesciunt vel, odit quos aliquid suscipit quo, similique perspiciatis ad. Veritatis, natus.</span>
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default Paciente;
